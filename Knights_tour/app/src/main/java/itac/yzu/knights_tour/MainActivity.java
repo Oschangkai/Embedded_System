a405fde69de96beea58940fe69337e6a718ad51f
Oschangkai <oschangkai@gmail.com>
2017-10-12 21:51:33 +0800

    Set TextView Background

--- Knights_tour/app/src/main/java/itac/yzu/knights_tour/MainActivity.java
+++ Knights_tour/app/src/main/java/itac/yzu/knights_tour/MainActivity.java
@@ -2,12 +2,35 @@ package itac.yzu.knights_tour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView[] textViews = new TextView[64];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set textView background color
+        TableLayout tb = (TableLayout)findViewById(R.id.tb);
+        for(int i = 0; i < 8; i++) {
+            TableRow tr = (TableRow)tb.getChildAt(i);
+            for(int j = 0; j < 8; j++) {
+                TextView tv = (TextView)tr.getChildAt(j);
+                if(i%2 == 0) {
+                    if(j%2 == 0)
+                        tv.setBackgroundResource(android.R.color.darker_gray);
+                } else {
+                    if(j%2 == 1)
+                        tv.setBackgroundResource(android.R.color.darker_gray);
+                }
+            }
+        }
     }
 }
