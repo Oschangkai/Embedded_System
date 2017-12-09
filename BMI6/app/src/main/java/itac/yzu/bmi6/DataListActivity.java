a644842f5fe5c9c768ed0aa60070bcea641aa20b
Oschangkai <oschangkai@gmail.com>
2017-12-10 01:26:23 +0800

    Init Lab9

---
+++ BMI6/app/src/main/java/itac/yzu/bmi6/DataListActivity.java
@@ -0,0 +1,13 @@
+package itac.yzu.bmi6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_list);
+    }
+}
